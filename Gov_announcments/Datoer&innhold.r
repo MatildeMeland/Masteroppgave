73cd26e32de59d923b30a62399a6fa163cc5f5d0
MatildeMeland <Matilde-Meland@hotmail.com>
2020-10-14 09:20:46 +0200

    Small changes

--- Gov_announcments/Datoer&innhold.r
+++ Gov_announcments/Datoer&innhold.r
@@ -13,7 +13,7 @@ raw <- raw %>% str_replace_all(c(" 2020" = "2020",
                           ," oktober" = "10."," november" = "11."," desember" = "12."))
 
 # Data frame where each column contains seperate information, each row a new announcement
-df <- as.data.frame(matrix(raw, ncol = 5, byrow = TRUE))
+df <- as.data.frame(matrix(raw, ncol = 5, byrow = T))
 
 # Keep first 3 colums, only ones with relevant information
 df <- df[,1:3]
@@ -22,10 +22,10 @@ df <- df[,1:3]
 colnames(df) <- c("date", "title", "content")
 
 # Remove date from title
-df[,2] <- gsub("\\(.+","",df[,2],ignore.case=T)
+df[,2] <- gsub("\\(.+", "", df[,2], ignore.case = T)
 
 # Create date format
-df$date <- as.Date(df$date,format="%d.%m.%Y")
+df$date <- as.Date(df$date, format="%d.%m.%Y")
 
 # Some problems due to some announcements containing "les mer" and others do not.
 # Some problems due to some articles not providing a link 
