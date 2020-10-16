b82b9b75bfb5e190638ab9f4bd601b9265e00a06
MatildeMeland <Matilde-Meland@hotmail.com>
2020-10-16 16:32:15 +0200

    stuff

--- NRK/News-script.R
+++ NRK/News-script.R
@@ -16,18 +16,18 @@ news_data <-
     sentences_count = `Sentence Count`,
     subject = Subjekt,
     url = Url,
-    word_count = Wordcount
-  )
+    word_count = Wordcount)
 
 news_data <-
   news_data %>% 
   select(date, title, pageviews, pageviews_mobile, xl, read_time, read_time_total,
          sentences_count, subject, url, word_count)
 
-# FIX - lage dummy variabel med 0/1 for xl artikler
+# Dummy varible for XL articles
+news_data$xl <- ifelse(news_data$xl == 'XL', 1, 0)
 
 # Remove all articles that doesn't have subject
-news_data_na <- subset(news_data, !is.na(news_data$subject))
+news_data <- subset(news_data, !is.na(news_data$subject))
 
 # Creates a new subset of all articles with only subject is corona
 corona_only_news <- subset(news_data, subject == "Nytt koronavirus (Covid-19)")
@@ -40,25 +40,34 @@ sum(corona_news$pageviews) # Total veiws on Corona articles (VG.no had 515M whil
 plot(table(corona_news$date))
 
 # More advanced plot
-table_corona <- as.data.frame(table(corona_news$date))
-table_corona$Var1 <- as.Date(table_corona$Var1)
+table_corona_clicks <- as.data.frame(table(corona_news$date))
+table_corona_clicks$Var1 <- as.Date(table_corona$Var1)
 
-ggplot(table_corona, aes(x = Var1, y = Freq)) + 
+ggplot(table_corona_clicks, aes(x = Var1, y = Freq)) + 
   geom_point()
 
-ggplot(data = table_corona, aes(x = Var1, y = Freq)) +
+ggplot(data = table_corona_clicks, aes(x = Var1, y = Freq)) +
   geom_bar(stat = "identity", fill = "lightblue") +
   labs(title = "Amount of Corona Articles by NRK",
        subtitle = "October 2019 - September 2020",
        x = "Date", y = "Number of Articles") +
-  scale_x_date(date_labels = "%d %b %Y") + # FIX 
+  scale_x_date(date_labels = "%d %b %Y") + # FIX date (only 3 months show up suddenly?)
   theme_bw()
 
 # Making a plot of reading time of corona articles over time
-## FIX - gjøre om total tid til timer
+# Changing the total time from seconds to hours
+corona_news$read_time_total <- corona_news$read_time_total/360
+
 ## Fix - lage tabel over dato og sammenlagt tid for alle artikler på en dag
+table_corona_time <- corona_news %>% 
+  select(date, read_time_total) %>% 
+  group_by(date) %>% 
+  summarise(time.sum = sum(read_time_total))
+
 ggplot(corona_news, aes(x = date, y = read_time_total)) + 
   geom_point()
+
+
 
 
 # Analzing the different subjects
@@ -80,3 +89,5 @@ table(subjects_all) %>%
   head(100)
 
 table_sub <- as.data.frame(table(subjects_all))
+
+# FIXX se på temaer som er relatert til finans, lage tidslinje
