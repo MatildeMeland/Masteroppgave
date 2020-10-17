0256a675b1deb758e35a5880fd794b0dbc2e29d5
Odin Mjølsnes <odin.mjolsnes@gmail.com>
2020-10-17 10:32:15 +0200

    Graphs of volume over time

--- Stock_data/stock_analysis2.R
+++ Stock_data/stock_analysis2.R
@@ -34,6 +34,16 @@ df$AV_ODEEN <- df$PX_VOLUME/(rollsumr(df$PX_VOLUME, k = 30, fill = NA)/30)
 df$AV_DV <- log(((df$PX_VOLUME + lead(df$PX_VOLUME))/2)+1) - log(((rollsumr(df$PX_VOLUME, k = 41, fill = NA)-rollsumr(df$PX_VOLUME, k = 11, fill = NA))/30)+1)
 
 
+# Calculating market return
+# - weight each stock by market cap?
+# - return of each stock
+test <- df %>% 
+  group_by(Security) %>%
+  summarize(mkt_cap_share = sum(CUR_MKT_CAP, na.rm = T)) %>% ungroup %>%
+  remove_missing()
+
+test$weight <- test$mkt_cap_share/sum(test$mkt_cap_share)
+sum(test$avg_mkt_cap)
 
 # Graph of trading volume over time
 df %>% 
@@ -53,7 +63,7 @@ grouped_df <- df %>%
   summarize(avg_mkt_cap = mean(CUR_MKT_CAP, na.rm = T)) %>%
   remove_missing() %>% 
   mutate(Size_Level = cut(avg_mkt_cap,
-                           quantile(avg_mkt_cap, c(0, .25, .75, 1)),
+                           quantile(avg_mkt_cap, c(0, .50, .75, 1)),
                            labels = c('Small', 'Medium', 'Big'),
                            include.lowest = TRUE)) %>% 
   merge(.,df, by = "Security")  # idk if this is the most efficient way..
@@ -71,9 +81,18 @@ grouped_df %>%
   theme_bw()
 
 # Could it be that most of this variation in volume is caused by Norwegian?
-test <- df %>% 
+tot_vol <- df %>% 
   group_by(Security) %>% 
   summarize(total_volume = sum(PX_VOLUME))
   # No.. KOA has had most trades over the period.
 
+tot_vol2 <- df %>% 
+  group_by(Security) %>%
+  summarize(avg_mkt_cap = mean(CUR_MKT_CAP, na.rm = T)) %>%
+  remove_missing() %>% 
+  mutate(Size_Level = cut(avg_mkt_cap,
+                          quantile(avg_mkt_cap, c(0, .25, .75, 1)),
+                          labels = c('Small', 'Medium', 'Big'),
+                          include.lowest = TRUE)) %>% 
+  merge(.,tot_vol, by = "Security")
 
