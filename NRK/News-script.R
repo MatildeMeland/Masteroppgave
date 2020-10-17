136b5025e896c8387d60e5be9b0a5fb498a298b5
MatildeMeland <Matilde-Meland@hotmail.com>
2020-10-17 19:27:41 +0200

    Last last last update

--- NRK/News-script.R
+++ NRK/News-script.R
@@ -174,6 +174,7 @@ test_finance[sample(nrow(test_finance), 20), 1:2]
 # Subsetting with all chosen finance subjects
 news_finance <- news_data[grep("trump|økonomi|teknologi|energi|reiseliv|politikk", news_data$subject, ignore.case = T), ]
 #news_finance <- news_finance[grep("", news_data$full_text, ignore.case = T), ] # When we add full text we can add words that have to be included
+#news_finance <- news_finance[-grep("", news_data$full_text, ignore.case = T), ] # Words we want to exclue - maybe we want to remove articles which mentions corona 5 or more times or something?
 
 # Random sample of articles form this sample (to check that they match)
 news_finance[sample(nrow(news_finance), 20), 1:2]
@@ -182,7 +183,8 @@ news_finance[sample(nrow(news_finance), 20), 1:2]
 table_finance <- as.data.frame(table(news_finance$date))
 table_finance$Var1 <- as.Date(table_finance$Var1)
 
-# Histogram
+# Plotting finance articles -----------------------------------------------
+# Number of finance articles by day
 ggplot(data = table_finance, aes(x = Var1, y = Freq)) +
   geom_bar(stat = "identity", fill = "lightblue") +
   labs(title = "Amount of Articles by NRK on Finance Related Topics",
@@ -190,13 +192,14 @@ ggplot(data = table_finance, aes(x = Var1, y = Freq)) +
        x = "Date", y = "Number of Articles") +
   scale_x_date(date_labels = "%d %b %Y",date_breaks  ="1 month") +
   theme_bw() +
-  theme(axis.text.x = element_text(angle = 60, hjust = 1))
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
 # Clicks on finance articles
 ggplot(news_finance, aes(x = date, y = pageviews)) + 
   geom_point() # Remove outliers?
 # "Spørsmål og svar om koronautbruddet" - This article has insane hits, think we should remove
 
+# Total number of pageviews by day of finance news
 news_finance %>% 
   select(date, pageviews) %>% 
   group_by(date) %>% 
@@ -206,9 +209,28 @@ news_finance %>%
   geom_bar(stat = "identity", fill = "lightblue") +
   geom_line() +
   geom_smooth(method = "lm", color = "darkgray") +
-  labs(title = "Total pageviews of Finance Articles by NRK (MILL)",
+  labs(title = "Total Pageviews of Finance Articles by NRK (MILL)",
        subtitle = "October 2019 - September 2020",
-       x = "Date", y = "Million pageviews") +
+       x = "Date", y = "Million Pageviews") +
   scale_x_date(date_labels = "%d %b %Y",date_breaks  ="1 month") +
   theme_bw() +
   theme(axis.text.x = element_text(angle = 60, hjust = 1))
+
+# Total number of pageviews by day of finance news
+news_finance %>% 
+  select(date, read_time) %>% 
+  group_by(date) %>% 
+  summarise(readtime.sum = sum(read_time)) %>%
+  
+  ggplot(., aes(x = as.Date(date), y = readtime.sum/(360))) +
+  geom_bar(stat = "identity", fill = "lightblue") +
+  geom_line() +
+  geom_smooth(method = "lm", color = "darkgray") +
+  labs(title = "Total Read Time of Finance Articles by NRK (hours)",
+       subtitle = "October 2019 - September 2020",
+       x = "Date", y = "Hours of Read Time") +
+  scale_x_date(date_labels = "%d %b %Y",date_breaks  ="1 month") +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 60, hjust = 1))
+
+
