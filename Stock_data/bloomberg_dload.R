d2ed28dda4d7b1202228751dfa3dd2d3117f7e03
Odin Mjølsnes <odin.mjolsnes@gmail.com>
2020-10-27 14:54:17 +0100

    added accounting variables

--- Stock_data/bloomberg_dload.R
+++ Stock_data/bloomberg_dload.R
@@ -8,7 +8,7 @@ ticker_date <- read_excel("Stock_data/ticker_date.xlsx")
 
 # Create a dataframe that repeats the date sequence 1 time for each company.
 # A total of 257 companies
-Security <- rep(ticker_date$input[1:257], times=1, each=length(ticker_date$date))
+Security <- rep(ticker_date$input[1:255], times=1, each=length(ticker_date$date))
 
 ticker_date2 <- as.data.frame(Security)
 
@@ -20,7 +20,17 @@ ticker_date2$PX_OPEN <- NA         # open stock price
 ticker_date2$PX_VOLUME <- NA       # trading volume
 ticker_date2$VOLATILITY_30D <- NA  # 30 day volatility
 ticker_date2$EQY_SH_OUT <- NA      # number of shares
-ticker_date2$CUR_MKT_CAP <- NA      # CUR_MKT_CAP
+ticker_date2$CUR_MKT_CAP <- NA     # CUR_MKT_CAP
+
+# Accounting variables
+ticker_date2$MARKET_CAPITALIZATION_TO_BV <- NA         # Market cap to book value
+ticker_date2$BOOK_VAL_PER_SH <- NA                     # Book value per share
+ticker_date2$PX_TO_BOOK_RATIO <- NA                    # Price to book ratio
+ticker_date2$RETURN_ON_INV_CAPITAL <- NA               # Return on invested capital...
+ticker_date2$TOTAL_ANALYST_REC <- NA                   # Total number of analyst recommendations 
+ticker_date2$PE_RATIO <- NA                            # Price per earning ratio
+ticker_date2$EQY_INST_PCT_SH_OUT <- NA                 # Institutional ownership percentage of outstanding shares
+
 
 
 
